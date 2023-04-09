9b1be0b25c5af76eb82aaf7a7611c4686d7af8b1
netman <pshindekov@gmail.com>
2023-04-09 13:05:12 -0700

    stage1

--- test1.sh
+++ test1.sh
@@ -1,2 +1,8 @@
 echo hellooooooooooooooooo
 echo llooloo0000o
+python3 -m pip install --upgrade pip
+python3 -m pip install ncclient
+python3 -m pip install pandas
+python3 -m pip install ipaddress
+python3 -m pip install netaddr
+python3 -m pip install prettytable
