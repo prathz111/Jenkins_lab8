20e5b9b453c48e1321fb77b24381703c2f2da543
netman <pshindekov@gmail.com>
2023-04-10 13:00:35 -0700

    t5

--- test1.sh
+++ test1.sh
@@ -1,4 +1,4 @@
-echo Installing ncclient, pandas, ipaddress, netaddr, prettytable
+echo Installing ncclient, pandas, ipaddress, netaddr, prettytable 
 python3 -m pip install --upgrade pip
 python3 -m pip install ncclient
 python3 -m pip install pandas
